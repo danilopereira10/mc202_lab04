a9f83cd4cf747a262a6ae3df977b374e984a3cfa
danilopereira10 <54330234+danilopereira10@users.noreply.github.com>
2019-09-22 17:51:02 -0300

    Bad version of lab04
    Old version, but that really prints reach characters

--- lab04.c
+++ lab04.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int chegouAoFimDaLinha(int i, int n) {
-	if (i % (n - 1) == 0) {
-		return 1;	
-	}
-	return 0;
-}
-
-void imprimeMatriz(int m, int n, char** matriz) {
+void imprimeMatriz(int m, int n, char matriz[][n]) {
 	for (int i = 0; i < m; i++) {
 		printf(matriz[i]);	
 		printf("\n");
@@ -24,22 +17,15 @@ int main() {
 	//char direction3;
 	//char direction4;
 	//char matriz[m][n];
-	char** matriz;
-	char* linha;
-	matriz = malloc(m * sizeof(linha));
 	for (int i = 0; i < m; i++) {
-		linha = malloc(n * sizeof(char));
 		for (int j = 0; j < n; j++) {
-			scanf("%c", &linha[j]);		
+			scanf("%c", &matriz[i][j]);		
 		}
-		matriz[i] = linha;
-		printf(linha);
-		printf("\n");	
 	}
 
 
-	
-	//imprimeMatriz(m, n, matriz);
+
+	imprimeMatriz(m, n, matriz);
 
 
 	return EXIT_SUCCESS;
